800508619bbf6675a2e12cfdb93959b316fbc545
avrdan <avrdan@gmail.com>
2014-02-15 19:09:06 +0100

    Fixed coords for hand collider.

--- ngj14/Assets/ScreenHandConverter.cs
+++ ngj14/Assets/ScreenHandConverter.cs
@@ -28,13 +28,15 @@ public class ScreenHandConverter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 v = mainCamera.WorldToScreenPoint(transform.position);
-		Vector3 screen = guiCamera.ScreenToWorldPoint(v);
+		Vector3 vx = new Vector3 (v.x, v.y, enemySpawn.transform.position.z - guiCamera.transform.parent.position.z);
+		Vector3 screen = guiCamera.ScreenToWorldPoint(vx);
 		Vector3 l = screen - guiCamera.transform.position;
 		Vector3 l0 = guiCamera.transform.position;
 		Vector3 n = new Vector3 (0.0f,0.0f,1.0f);
 		Vector3 p0 = l0 + new Vector3(0.0f,0.0f,-enemySpawn.transform.position.z);
 		float d = Vector3.Dot((p0 - l0),n)/Vector3.Dot(l,n); 
 		collider.transform.position = l0 + d * l;
+		collider.transform.position = screen;
 	}
 
 	void OnDrawGizmos()
