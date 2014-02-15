0bce99ac810bc9b4d5e549567d97cacf77957e2b
Bilal Arslan <bilalarslan11@gmail.com>
2014-02-15 20:18:56 +0100

    Eyes
    In progress

---
+++ ngj14/Assets/EyeballAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeballAnimation : MonoBehaviour 
+{
+
+	private GameObject hand; 
+	private GameObject guiCamera;
+
+	void Start () 
+	{
+		hand = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update () 
+	{
+		//transform.LookAt(hand.transform);
+		transform.rotation = 
+		if (transform.eulerAngles.x > 90)
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x, 90f, 90f);
+		else
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x, -90f, -90f);
+	}
+}
