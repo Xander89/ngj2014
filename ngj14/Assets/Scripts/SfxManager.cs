c307d3fff928bd1f6db1195901c16a3031f3b6af
Alex <xander890@gmail.com>
2014-02-15 13:29:52 +0100

    +sound stuff

---
+++ ngj14/Assets/Scripts/SfxManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SfxManager : MonoBehaviour {
+
+	[Range(0.0f,1.0f)]
+	float sfxVolume = 1.0f;
+
+	[System.Serializable]
+	class ActiveSource
+	{
+		public AudioSource source;
+		public bool busy;
+	}
+
+	private List<ActiveSource> sources;
+//	private Dictionary<string, AudioClip> clips;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource[] o_sources = GetComponentsInChildren<AudioSource>();
+		sources = new List<ActiveSource>();
+		foreach(AudioSource si in o_sources)
+		{
+			ActiveSource a = new ActiveSource();
+			a.busy = false;
+			a.source = si;
+			a.source.volume = sfxVolume;
+			sources.Add(a);
+		}
+		//StartCoroutine(Test());
+	}
+	
+	// Update is called once per frame
+	public void PlaySfx (string name) {
+		AudioClip a = Resources.Load<AudioClip>(name);	
+		ActiveSource s = getFirstNotBusyAudioSouce();
+		if(s != null)
+		{
+			s.busy = true;
+			s.source.clip = a;
+			s.source.Play();
+			StartCoroutine(debusy(s));
+		}
+		else
+		{
+			Debug.Log("[Sound] Everything is busy, not playing " + name + " Audio file");
+		}
+	}
+
+	private IEnumerator debusy(ActiveSource s) 
+	{
+		yield return new WaitForSeconds(s.source.clip.length);
+		s.busy = false;
+	}
+ 
+	private ActiveSource getFirstNotBusyAudioSouce()
+	{
+		foreach(ActiveSource s in sources)
+		{
+			if(!s.busy)
+			{
+				return s;
+			}
+		}
+		return null;
+	}
+
+	private IEnumerator Test()
+	{
+		while(true)
+		{
+			PlaySfx("sfx_mockup");
+			yield return new WaitForSeconds(Random.Range(0.0f,1.0f));
+		}
+	}
+}
