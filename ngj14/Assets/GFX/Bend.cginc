1d1914a14f290ccaf5641e393a7d648f9f5091a0
Bilal Arslan <bilalarslan11@gmail.com>
2014-02-16 09:47:50 +0100

    LOOK AT THE PAPER!

--- ngj14/Assets/GFX/Bend.cginc
+++ ngj14/Assets/GFX/Bend.cginc
@@ -10,7 +10,7 @@ inline float4 bend_ObjectToClip(float4 vertex)
 	float4 pos = mul (UNITY_MATRIX_MVP, vertex);
 	float z = pos.w;
 //	pos.y = pos.y + z * z * (BEND_AMOUNT);// _Bend;
-	pos.y = pos.y + z * z * _Bend;
+	pos.x = pos.x + z * z * _Bend;
 	return pos;
 }	
 
--- ngj14/Assets/Scripts/Hand.cs
+++ ngj14/Assets/Scripts/Hand.cs
@@ -5,29 +5,71 @@ public class Hand : MonoBehaviour {
 
 
 	public Renderer _renderer;
+	public SpriteFade red;
+	private GUIText _message;
 	private bool _restart = false;
 	private float time = 0;
 
+	void Start()
+	{
+		_message = red.transform.FindChild("message").GetComponent<GUIText>();
+	}
 
 	void Update()
 	{
 		time += Time.deltaTime;
-		if (time < 2)
+
+		if (time < 3)
+		{
+			if (_renderer.isVisible)
+			{
+				_restart = false;
+				_message.enabled = false;
+				SpriteRenderer redflash = red.GetComponent<SpriteRenderer>();
+				redflash.color = new Color(redflash.color.r, redflash.color.g, redflash.color.b, 0f);
+				red.enabled = false;
+			}
 			return;
+		}
 
 		if (!_restart) {
 			if (!_renderer.isVisible) {
-					Debug.Log ("fade it");
-				Fade.FadeOut ();
+				StartCoroutine(BlinkRed());
 				_restart = true;
 				time = 0;
 				
 			}
 		}
+
+
+
+		else if (_restart && time > 3 && time < 5)
+		{
+			Debug.Log ("fade it");
+			Fade.FadeOut ();
+			return;
+		}
+			
 		else
 		{
 			Application.LoadLevel(0);
 		}
+	}
+
+	IEnumerator BlinkRed()
+	{
+		red.enabled = true;
+		_message.enabled = true;
+		Debug.Log ("DUAHDUAW");
+		red.StartFading(0.8f);
+		yield return new WaitForSeconds(0.8f);
+		red.StartFading(0f);
+		yield return new WaitForSeconds(0.8f);
+		red.StartFading(0.8f);
+		yield return new WaitForSeconds(0.8f);
+		red.StartFading(0f);
+		Debug.Log ("DUAHDUAW2");
+		_message.enabled = false;
 	}
 
 }
